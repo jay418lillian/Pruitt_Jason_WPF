e500ce039053e428fa160c8abba04ae1b55d23d1
JasonPruitt <jay418lillian@hotmail.com>
2015-02-11 09:05:14 -0500

    First commit for GTT

---
+++ Pruitt_Jason_Expressions_Assignment/JS/script.js
@@ -0,0 +1,21 @@
+/**
+ * Jason Pruitt
+ * SDI 1502
+ * Section 1
+ *
+ */
+
+//Variables and Data Types
+//var myAddress = "123 Elm Street";
+//var milesToStore = 15;
+//var isThereConstruction = true;
+var myFirstName;
+var myLastName;
+
+
+// Prompts
+myFirstName = prompt("Please enter your first name: ");
+myLastName = prompt("Please enter your last name: ");
+
+// Outputs
+console.log("You entered the following name: " + myFirstName + " " + myFirstName + ".");
