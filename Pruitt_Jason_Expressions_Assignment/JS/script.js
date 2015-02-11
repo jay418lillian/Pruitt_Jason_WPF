f1f77b5de32c6d938df5c3c897c74e2bc422db60
JasonPruitt <jay418lillian@hotmail.com>
2015-02-11 11:00:03 -0500

    order of operations
    perimeter

--- Pruitt_Jason_Expressions_Assignment/JS/script.js
+++ Pruitt_Jason_Expressions_Assignment/JS/script.js
@@ -14,6 +14,9 @@ var quiz4 = 80;
 // the average is the sum of the items divided by the number of items
 var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
 
+var length = 7;
+var width = 6;
+var perimeter = length *2 + width * 2;
 
 // Prompts
 
@@ -22,5 +25,5 @@ var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
 
 
 // Outputs
-
-console.log(average);
+console.log(perimeter);
+//console.log(average);
