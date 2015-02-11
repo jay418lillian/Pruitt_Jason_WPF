2cc06527447e16df523ff56a7e89aa709cabe509
JasonPruitt <jay418lillian@hotmail.com>
2015-02-11 10:57:41 -0500

    Order of operations
    PEMDAS

--- Pruitt_Jason_Expressions_Assignment/JS/script.js
+++ Pruitt_Jason_Expressions_Assignment/JS/script.js
@@ -4,10 +4,15 @@
  * Section 1
  *
  */
-
+// PEMDAS
 //Variables and Data Types
-var reamainder = 32 % 10;
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
 
+// the average is the sum of the items divided by the number of items
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
 
 
 // Prompts
@@ -18,4 +23,4 @@ var reamainder = 32 % 10;
 
 // Outputs
 
-console.log(reamainder);
+console.log(average);
