d39dbf739966c935e7f49e4705cff0c89ecf137a
JasonPruitt <jay418lillian@hotmail.com>
2015-02-11 12:19:04 -0500

    Dog Years
    Sparky’s Age

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,32 @@
+/**
+ * Jason Pruitt
+ * SDI 1502
+ * Section 1
+ * 2-11-15
+ * Expressions WorkSheet
+ */
+
+
+//Variables and Data Types
+//Calculate Sparky's Age
+var age;
+var dogYears;
+//Multiply age times 7 to obtain dog years.
+age = 4;
+dogYears = age * 7;
+
+
+
+
+
+// Prompts
+
+
+
+
+
+
+
+
+// Outputs
+console.log("Sparky's age in dog years is " + dogYears);
