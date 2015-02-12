b72012a4a541ba8c1304d07af401f829de9a3128
JasonPruitt <jay418lillian@hotmail.com>
2015-02-12 08:55:44 -0500

    Average Groceries

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -6,23 +6,18 @@
  * Expressions WorkSheet
  */
 
-
-//Variables and Data Types
-
 //Calculate Sparky's Age
 
 var age;
 var dogYears;
-
 
 //Multiply age times 7 to obtain dog years.
 
 age = 4;
 dogYears = age * 7;
 
-
-// Outputs
 //Print out the Age of Sparky in Dog Years.
+
 console.log("Sparky is " + age + " in human years, which is " + dogYears + " in dog years.");
 
 //Calculate how many slices of pizza everyone will receive.
@@ -38,6 +33,7 @@ people = 15;
 slicesPerPerson = (pizzas * slices) / people;
 
 //Each person receives X slices of pizza at the party.
+
 console.log("Each person ate " + slicesPerPerson + " slices of pizza at the party.");
 
 //Calculate how many left over pieces of pizza Sparky gets to eat.
@@ -46,7 +42,20 @@ var sparkySlices;
 
 sparkySlices = (pizzas * slices) % people;
 
-
 //Print Sparky got X slices of pizza.
 
 console.log("Sparky got " + sparkySlices + " slices of pizza.");
+
+//Calculate the average cost of Groceries over the last 5 weeks.
+
+var groceryList;
+var average;
+var total;
+
+groceryList = [125.89,202.33,99.99,147.01,174.63];
+total = groceryList[0] + groceryList[1] + groceryList[2] + groceryList[3] + groceryList[4];
+average = total / 5;
+
+//You have spent a total of $X on groceries over 5 weeks. That is an average of $X per week
+
+console.log("You have spent a total of " + total + " on groceries over 5 weeks. That is an average of " + average + " per week.")
