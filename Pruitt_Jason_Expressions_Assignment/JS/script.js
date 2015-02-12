3344bb6010dadca842fdfa91484a6c25115156c7
JasonPruitt <jay418lillian@hotmail.com>
2015-02-12 13:05:13 -0500

    Added Correct Array
    working properly, finally.

--- Pruitt_Jason_Expressions_Assignment/JS/script.js
+++ Pruitt_Jason_Expressions_Assignment/JS/script.js
@@ -7,11 +7,12 @@
 var apples = prompt("I am calculating how much fruit you consume in a month. How many apples do you eat in a week?") * 4;
 var bananas = prompt("How many bananas do you eat every two weeks?") * 2;
 var oranges = prompt("How many oranges do you eat in a week?") * 4;
-var fruit = apples + bananas + oranges;
+var fruit = [apples, bananas, oranges];
+var fruitTotal = fruit[0] + fruit[1] + fruit[2];
 
 console.log(apples);
 console.log(bananas);
 console.log(oranges);
 console.log(fruit);
 
-alert("Wow, you are projected to eat " + fruit + " pieces of fruit this month!");
+alert("Wow, you are projected to eat " + fruitTotal + " pieces of fruit this month!");
