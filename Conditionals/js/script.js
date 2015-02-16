04e2caab4dab6b3e8f7a2bcdb1eca7e407cc451c
JasonPruitt <jay418lillian@hotmail.com>
2015-02-15 23:58:13 -0500

    Conditionals
    console.log understanding

---
+++ Conditionals/js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Jason on 2/12/15.
+ */
+
+var oldEnough = false;
+
+//if the child is old enough, print to console " you can ride."
+
+if(oldEnough){
+    //code performed if conditional is true
+    console.log("You can ride the coaster!");
+}
