ae30f8949736b94e4bda1debd90aee7304c72c4c
JasonPruitt <jay418lillian@hotmail.com>
2015-02-16 11:59:34 -0500

    if else

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -2,9 +2,9 @@
  * Created by Jason on 2/12/15.
  */
 
-var kidHeight = 40;
+var kidHeight = 45;
 var minHeight = 48;
-
+var parentHeight = 45;
 //if the child is old enough, print to console " you can ride."
 //if kid is over 48 inches in height
 
@@ -12,6 +12,9 @@ if(kidHeight > minHeight){
     //code performed if conditional is true
     console.log("You can ride the coaster!");
 }
-if(kidHeight <= minHeight){
+else if(kidHeight >= parentHeight){
+    console.log("You can ride, but only with a parent present.");
+}else{
     console.log("Sorry kid, you've got some growing to do first.");
+
 }
