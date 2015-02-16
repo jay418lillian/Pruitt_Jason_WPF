ad78a41a3d6ba3c93ecafdcc5e74c612d4e7e5ce
JasonPruitt <jay418lillian@hotmail.com>
2015-02-16 11:51:04 -0500

    if and else
    two different blocks

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -2,11 +2,16 @@
  * Created by Jason on 2/12/15.
  */
 
-var kidHeight = 50;
+var kidHeight = 40;
 var minHeight = 48;
+
 //if the child is old enough, print to console " you can ride."
 //if kid is over 48 inches in height
+
 if(kidHeight > minHeight){
     //code performed if conditional is true
     console.log("You can ride the coaster!");
+}
+if(kidHeight <= minHeight){
+    console.log("Sorry kid, you've got some growing to do first.");
 }
