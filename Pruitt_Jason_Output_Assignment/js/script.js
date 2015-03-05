cd8cb721e713ed626ef9283339aef47ed2c8b8fa
JasonPruitt <jay418lillian@hotmail.com>
2015-03-05 16:11:04 -0500

    Deleted old Files & Started on Outputs

---
+++ Pruitt_Jason_Output_Assignment/js/script.js
@@ -0,0 +1,13 @@
+/**
+Jason Pruitt
+ 3-5-15
+ C201503
+ */
+
+var shotPercentage = 55.72;        //number data type
+var basketball = true;          //boolean data type
+var myName = "Jason Pruitt";    //string data type
+
+console.log("Hello, My name is " + myName + " and I am glad to be here!");   //
+console.log("");
+console.log("");
