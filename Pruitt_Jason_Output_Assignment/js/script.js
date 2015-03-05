95633d435febd691ecbcf1e9ad32de5e13f50cc4
JasonPruitt <jay418lillian@hotmail.com>
2015-03-05 16:22:21 -0500

    Aligned Comments and Updated My Information in js file

--- Pruitt_Jason_Output_Assignment/js/script.js
+++ Pruitt_Jason_Output_Assignment/js/script.js
@@ -1,13 +1,14 @@
 /**
 Jason Pruitt
  3-5-15
+ SDI
  C201503
  */
 
-var shotPercentage = 55.72;        //number data type
-var basketball = true;          //boolean data type
-var myName = "Jason Pruitt";    //string data type
+var shotPercentage = 55.72;         //number data type
+var basketball = true;              //boolean data type
+var myName = "Jason Pruitt";        //string data type
 
 console.log("Hello, My name is " + myName + " and I am glad to be here!");                                                  //concatenating using the string variable
 console.log("I enjoy playing basketball on the weekends, and have a shooting percentage of " + shotPercentage + "%.");      //concatenating the number data type to the console
-console.log("");
+console.log("I have played basketball all of my life and it is " + basketball + " that I play in an adult league.");        //concatenating using the boolean value
