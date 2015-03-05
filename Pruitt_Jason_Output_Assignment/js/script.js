a83d8456f870166c2fbce35154e7f08fc9afc3ad
JasonPruitt <jay418lillian@hotmail.com>
2015-03-05 16:15:29 -0500

    Concatenating Number Value to Console.log

--- Pruitt_Jason_Output_Assignment/js/script.js
+++ Pruitt_Jason_Output_Assignment/js/script.js
@@ -8,6 +8,6 @@ var shotPercentage = 55.72;        //number data type
 var basketball = true;          //boolean data type
 var myName = "Jason Pruitt";    //string data type
 
-console.log("Hello, My name is " + myName + " and I am glad to be here!");   //
-console.log("");
+console.log("Hello, My name is " + myName + " and I am glad to be here!");                                                  //concatenating using the string variable
+console.log("I enjoy playing basketball on the weekends, and have a shooting percentage of " + shotPercentage + "%.");      //concatenating the number data type to the console
 console.log("");
