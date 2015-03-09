9249b31ccee2094e6e1e6888f6eb0e0d181b7b80
JasonPruitt <jay418lillian@hotmail.com>
2015-03-09 18:55:48 -0400

    Created variables for configuring the area of a rectangle

--- Week 2 Activities/Prompts and Alerts/js/script.js
+++ Week 2 Activities/Prompts and Alerts/js/script.js
@@ -7,5 +7,7 @@ Week 2 Activities
  */
 
     //Prompt & Alert
-var userInput = prompt("Enter your year of birth");
+var width = prompt("We are calculating the area of a recatangle. PLease enter the width.");
+var height = prompt("Please enter the height.");
+
 console.log(userInput);
