e05310575b1e9d7d84cfbb6a0288908240d47b39
JasonPruitt <jay418lillian@hotmail.com>
2015-03-09 18:59:17 -0400

    confirming formula works

--- Week 2 Activities/Prompts and Alerts/js/script.js
+++ Week 2 Activities/Prompts and Alerts/js/script.js
@@ -7,7 +7,7 @@ Week 2 Activities
  */
 
     //Prompt & Alert
-var width = prompt("We are calculating the area of a recatangle. PLease enter the width.");
+var width = prompt("We are calculating the area of a recatangle. \n Please enter the width.");
 var height = prompt("Please enter the height.");
-
-console.log(userInput);
+var area = width * height;      //calculates with that info
+console.log(area);              //prints it out to console
