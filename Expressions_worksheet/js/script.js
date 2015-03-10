0ef6cd4968b957863ce87b37bc2f86a6a5ef2260
JasonPruitt <jay418lillian@hotmail.com>
2015-03-10 16:19:00 -0400

    Slice of Pie Part 2

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -4,9 +4,8 @@ Jason Pruitt
 SDI
  C201503
 Expressions Worksheet
- Dog Years
  */
-
+//Dog Years
 //Calculate how old Sparky the pit bull is in dog years.
 
 var dogAge = 6;
@@ -18,3 +17,27 @@ var dogYears = dogAge * 7;
 //Print output to console
 
 console.log("Sparky is " + dogAge + " human years old which is " + dogYears + " in dog years.");
+
+//Slice of Pie part 1
+//Calculate how many slices of Pizza each person receives.
+
+var slicesPerPizza = 8;     //given variable for slices per pizza
+var people = 9;            //given variable for number of people
+var pizzas = 6;             //given variable for number of pizzas
+
+//Multiply slicesPerPizza * pizzas and divide result by people.
+
+var slicesPerPerson = slicesPerPizza * pizzas / people;
+
+//Print slicesPerPerson to console.
+
+console.log("Each person ate " + slicesPerPerson + " slices of pizza at the party.");
+
+//Slice of Pie part 2
+//Calculate number of left over slices of pizza that Sparky will eat.
+
+var sparkysShare = slicesPerPizza * pizzas % people;
+
+//print result to console
+
+console.log("Sparky got " + sparkysShare + " slices of pizza.");
