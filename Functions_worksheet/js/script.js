c3165ddb6a8b81017a83ef266efeb60895a50336
JasonPruitt <jay418lillian@hotmail.com>
2015-03-24 18:04:15 -0400

    Example

---
+++ Functions_worksheet/js/script.js
@@ -0,0 +1,20 @@
+/**
+Jason Pruitt
+3-24-15
+SDI
+ C201503
+ Functions Worksheet
+ */
+
+    //Example
+    //Calculate the Area of a Rectangle
+var width = 5;
+var height = 6;
+var area = calcArea(width,height);
+
+function calcArea(w, h){
+
+ return w*h
+}
+
+console.log("The area of the rectangle is " + area);
