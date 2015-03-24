b1572e3cff7d0ea6ccc0b9ce60170b3422f7b16b
JasonPruitt <jay418lillian@hotmail.com>
2015-03-24 16:56:07 -0400

    Parameters and Arguments

--- Week 4 Activites/js/script.js
+++ Week 4 Activites/js/script.js
@@ -6,3 +6,11 @@ SDI
 Week 4 Activities
  */
 
+function dogYears(age){
+ var dogYears = age * 7;
+ console.log("Sparky is " + dogYears + " years old.");
+}
+
+var doggy = 50;
+dogYears(4);
+dogYears(doggy);
