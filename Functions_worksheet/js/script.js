6bce33ec3b1c9d5c0ea38e1cc58df0300900f212
JasonPruitt <jay418lillian@hotmail.com>
2015-03-24 18:56:22 -0400

    Circumference

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -8,13 +8,30 @@ SDI
 
     //Example
     //Calculate the Area of a Rectangle
+
 var width = 5;
 var height = 6;
 var area = calcArea(width,height);
 
 function calcArea(w, h){
 
- return w*h
+ return w*h;
 }
 
 console.log("The area of the rectangle is " + area);
+
+    //Circumference
+    //Calculate the Circumference of a Circle
+
+var radius = 5;
+var pie = 3.14;
+
+
+var circumference = calcCircle(radius,pie);
+
+function calcCircle(r,p){
+
+ return r * p * 2;
+}
+
+console.log("The circumference of the circle is " + circumference + ".");
